ce804f49370573a4d0f6aac45965a52e70646ba0
Tapan <tapan.mjo92@gmail.com>
2025-05-25 12:04:20 +0000

    Initial commit

---
+++ test/lambda-pulse-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaPulseBackend from '../lib/lambda-pulse-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-pulse-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaPulseBackend.LambdaPulseBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
